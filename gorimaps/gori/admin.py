df8770083c6c9748a23680db3d5caa5c1dbfcf6e
rezichaladze <rezichaladze@gmail.com>
2022-10-07 15:44:03 +0400

    ds

--- gorimaps/gori/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import History, Culture, Architect, AboutProjectDonors
-
-
-admin.site.register(History)
-admin.site.register(Culture)
-admin.site.register(Architect)
-admin.site.register(AboutProjectDonors)
