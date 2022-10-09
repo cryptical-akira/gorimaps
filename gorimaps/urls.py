271e7d7360310584b443ff2fb8df152d766eb0c1
rezichaladze <rezichaladze@gmail.com>
2022-10-09 22:57:22 +0400

    last commit

--- gorimaps/urls.py
+++ gorimaps/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, register_converter
 from gori.views import index_page, index_wout_lang, about_project, history, culture, architect
+from gori.views import history_detail_view, culture_detail_view, architect_detail_view
 from .converters import LanguagePathConverter
 
 
@@ -15,8 +16,12 @@ urlpatterns = [
     path('about-project/<lang:lang>/', about_project, name="about_project"),
 
     path('history/<lang:lang>/', history, name="history"),
+    path('history/<int:id>/<lang:lang>/', history_detail_view, name='history-detail'),
 
     path('culture/<lang:lang>/', culture, name="culture"),
+    path('culture/<int:id>/<lang:lang>/', culture_detail_view, name='culture-detail'),
 
     path('architect/<lang:lang>/', architect, name="architect"),
+    path('architect/<int:id>/<lang:lang>/', architect_detail_view, name='architect-detail'),
+
 ]
