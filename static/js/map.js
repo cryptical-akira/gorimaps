2743794122526bc7fe814708d8b3875b763de893
rezichaladze <rezichaladze@gmail.com>
2022-10-24 19:37:23 +0400

    mobile map functionall

--- static/js/map.js
+++ static/js/map.js
@@ -4,16 +4,21 @@ function initMap() {
         zoom: 16,
         center: { lat: pins[0].latitude, lng: pins[0].longitude },
     });
+    const maps = new google.maps.Map(document.getElementById("maps"), {
+        zoom: 16,
+        center: { lat: pins[0].latitude, lng: pins[0].longitude },
+    });
     const markers = pins.map(pin => {
         return [new google.maps.Marker({ position: { lat: pin.latitude, lng: pin.longitude, post: pin.post }, map: map }), pin.post]
+    });
+    const marker = pins.map(pin => {
+        return [new google.maps.Marker({ position: { lat: pin.latitude, lng: pin.longitude, post: pin.post }, map: maps }), pin.post]
     });
     // add event listener to each marker
     markers.forEach(marker => {
         marker[0].addListener("click", () => {
             const postdiv = document.getElementById("post");
             postdiv.innerHTML = "";
-
-            console.log()
 
             if (marker[1].video == ''){
                 postvisual = `<img src="/static${marker[1].img}" class="pinedpostimg">`;
@@ -33,13 +38,41 @@ function initMap() {
 
             </div>
             </div>`;
-            console.log(pins)
-            console.log(marker)
+            console.log(marker);
             document.getElementById("post").style.display = "block";
             document.getElementById("pinponstdiv").style.display = "block";
             document.getElementById("postback").style.display = "block";
         })
     })
-}
+    marker.forEach(marker => {
+        marker[0].addListener("click", () => {
+            const postdiv = document.getElementById("postmobile");
+            postdiv.innerHTML = "";
 
+            if (marker[1].video == ''){
+                postvisual = `<img src="/static${marker[1].img}" class="pinedpostimg">`;
+            }else{
+                postvisual = ` <iframe width="85%" height="170px" src="${marker[1].video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="margin-top:50px; margin-left:25px"></iframe>`;
+            }
+            postdiv.innerHTML = `
+            <div class="pinpostdiv" id="pinponstdiv">
+            <span class="pinpostclose" 
+            onclick="document.getElementById('postmobile').style.display = 'none'">
+            დახურვა</span>
+            <div class="postbkack">
+            <span class="pinpostheader">${marker[1].title}</span>
+            ${postvisual}
+            <span class="pinedpostbody">${marker[1].body}</span>
+            <a href="${marker[1].source}" target="_blank" style="margin-left:25px; display:block; color:black; margin-bottom: 50px">${marker[1].source}</a>
+
+            </div>
+            </div>`;
+            console.log(marker);
+            document.getElementById("postmobile").style.display = "block";
+            document.getElementById("pinponstdiv").style.display = "block";
+            document.getElementById("postback").style.display = "block";
+        })
+    })
+
+}
 window.initMap = initMap;
--- static/js/mapen.js
+++ static/js/mapen.js
@@ -4,16 +4,21 @@ function initMap() {
         zoom: 16,
         center: { lat: pins[0].latitude, lng: pins[0].longitude },
     });
+    const maps = new google.maps.Map(document.getElementById("maps"), {
+        zoom: 16,
+        center: { lat: pins[0].latitude, lng: pins[0].longitude },
+    });
     const markers = pins.map(pin => {
         return [new google.maps.Marker({ position: { lat: pin.latitude, lng: pin.longitude, post: pin.post }, map: map }), pin.post]
+    });
+    const marker = pins.map(pin => {
+        return [new google.maps.Marker({ position: { lat: pin.latitude, lng: pin.longitude, post: pin.post }, map: maps }), pin.post]
     });
     // add event listener to each marker
     markers.forEach(marker => {
         marker[0].addListener("click", () => {
             const postdiv = document.getElementById("post");
             postdiv.innerHTML = "";
-
-            console.log()
 
             if (marker[1].video == ''){
                 postvisual = `<img src="/static${marker[1].img}" class="pinedpostimg">`;
@@ -24,7 +29,7 @@ function initMap() {
             <div class="pinpostdiv" id="pinponstdiv">
             <span class="pinpostclose" 
             onclick="document.getElementById('post').style.display = 'none'">
-            დახურვა</span>
+            close</span>
             <div class="postbkack">
             <span class="pinpostheader">${marker[1].title_en}</span>
             ${postvisual}
@@ -38,6 +43,35 @@ function initMap() {
             document.getElementById("postback").style.display = "block";
         })
     })
-}
+    marker.forEach(marker => {
+        marker[0].addListener("click", () => {
+            const postdiv = document.getElementById("postmobile");
+            postdiv.innerHTML = "";
 
+            if (marker[1].video == ''){
+                postvisual = `<img src="/static${marker[1].img}" class="pinedpostimg">`;
+            }else{
+                postvisual = ` <iframe width="85%" height="170px" src="${marker[1].video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="margin-top:50px; margin-left:25px"></iframe>`;
+            }
+            postdiv.innerHTML = `
+            <div class="pinpostdiv" id="pinponstdiv">
+            <span class="pinpostclose" 
+            onclick="document.getElementById('postmobile').style.display = 'none'">
+            close</span>
+            <div class="postbkack">
+            <span class="pinpostheader">${marker[1].title_en}</span>
+            ${postvisual}
+            <span class="pinedpostbody">${marker[1].body_en}</span>
+            <a href="${marker[1].source}" target="_blank" style="margin-left:25px; display:block; color:black; margin-bottom: 50px">${marker[1].source}</a>
+
+            </div>
+            </div>`;
+            console.log(marker);
+            document.getElementById("postmobile").style.display = "block";
+            document.getElementById("pinponstdiv").style.display = "block";
+            document.getElementById("postback").style.display = "block";
+        })
+    })
+
+}
 window.initMap = initMap;
